93b41419c14274b1ffec3f210279551a3a60f608
youngpascal <wilburn.josha@gmail.com>
2020-03-06 11:18:47 -0600

    initial push for heroku

---
+++ scraper/app.py
@@ -0,0 +1,56 @@
+
+from spiders.covid_spider import CovidSpider
+import json
+
+from klein import route, run
+from scrapy import signals
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.project import get_project_settings
+
+
+class MyCrawlerRunner(CrawlerRunner):
+    """
+    Crawler object that collects items and returns output after finishing crawl.
+    """
+    def crawl(self, crawler_or_spidercls, *args, **kwargs):
+        # keep all items scraped
+        self.items = []
+
+        # create crawler (Same as in base CrawlerProcess)
+        crawler = self.create_crawler(crawler_or_spidercls)
+
+        # handle each item scraped
+        crawler.signals.connect(self.item_scraped, signals.item_scraped)
+
+        # create Twisted.Deferred launching crawl
+        dfd = self._crawl(crawler, *args, **kwargs)
+
+        # add callback - when crawl is done cal return_items
+        dfd.addCallback(self.return_items)
+        return dfd
+
+    def item_scraped(self, item, response, spider):
+        self.items.append(item)
+
+    def return_items(self, result):
+        return self.items
+
+def return_spider_output(output):
+    """
+    :param output: items scraped by CrawlerRunner
+    :return: json with list of items
+    """
+    # this just turns items into dictionaries
+    # you may want to use Scrapy JSON serializer here
+    return json.dumps([dict(item) for item in output])
+
+@route("/api")
+def schedule(request):
+    settings = get_project_settings()
+    runner = MyCrawlerRunner(settings)
+    deferred = runner.crawl(CovidSpider)
+    deferred.addCallback(return_spider_output)
+    return deferred
+
+
+run("localhost", 8080)
---
+++ scraper/spiders/covid_spider.py
@@ -0,0 +1,57 @@
+
+import scrapy
+
+from scrapy.spiders import Spider
+from scrapy.selector import Selector
+import re 
+
+class CountryTest(scrapy.Item):
+    name = scrapy.Field()
+    total_cases = scrapy.Field()
+    new_cases = scrapy.Field()
+    total_deaths = scrapy.Field()
+    new_deaths = scrapy.Field()
+    active_cases = scrapy.Field()
+    total_recovered = scrapy.Field()
+    condition = scrapy.Field()
+    percentage_changed_cases = scrapy.Field()
+    percentage_changed_deaths = scrapy.Field()
+    date_updated = scrapy.Field()
+
+class TotalData(scrapy.Item):
+    total_covid_cases = scrapy.Field()
+    total_covid_deaths = scrapy.Field()
+    total_covid_recovered = scrapy.Field()
+
+
+class CovidSpider(Spider):
+
+    name = "covid"
+    start_urls = [ 'https://www.worldometers.info/coronavirus/' ]
+
+  
+    def parse(self, response):
+        date = response.xpath("//div[contains(text(), 'Last update')]/text()").getall()
+        total_numbers = response.xpath("//div[@class='maincounter-number']/span/text()").getall()
+        totals = TotalData()
+        totals['total_covid_cases'] = total_numbers[0]
+        totals['total_covid_deaths'] = total_numbers[1]
+        totals['total_covid_recovered'] = total_numbers[2]
+        yield totals
+        country_rows = response.css('tr') #get the table row
+        for country in country_rows: 
+            c = country.css('td::text').getall() # dump the row text into a list
+            if len(c) == 8:
+                data = CountryTest()
+                data['name'] = c[0]
+                data['total_cases'] = int(''.join(re.findall("[0-9]", c[1]))) if c[1].strip() else 0
+                data['new_cases'] = int(''.join(re.findall("[0-9]", c[2]))) if c[2].strip() else 0
+                data['total_deaths'] = int(''.join(re.findall("[0-9]", c[3]))) if c[3].strip() else 0
+                data['new_deaths'] = int(''.join(re.findall("[0-9]", c[4]))) if c[4].strip() else 0
+                data['active_cases'] = int(''.join(re.findall("[0-9]", c[5]))) if c[5].strip() else 0
+                data['total_recovered'] = int(''.join(re.findall("[0-9]", c[6]))) if c[6].strip() else 0
+                data['condition'] = int(''.join(re.findall("[0-9]", c[7]))) if c[7].strip() else 0
+                data['percentage_changed_cases'] = (data['new_cases'] / (data['total_cases'] - data['new_cases'])) * 100 if data['total_cases'] != data['new_cases'] else 0
+                data['percentage_changed_deaths'] = (data['new_deaths'] / (data['total_deaths'] - data['new_deaths'])) * 100 if data['total_deaths'] != data['new_deaths'] else 0
+                data['date_updated'] = ''.join(date)
+                yield data
